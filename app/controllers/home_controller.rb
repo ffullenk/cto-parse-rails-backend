c884622491bb40c4334a04b538019308c12e7038
francisco <francisco000@gmail.com>
2013-09-23 10:37:34 -0300

    resultados de fotos en la busqueda

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,7 @@ class HomeController < ApplicationController
      @news = Array.new
      News.all.each do |a|
         query.each do |q|
-        if (a.texto.downcase.include? q.downcase)
+        if (a.texto.downcase.include? q.downcase) or (a.resumen.downcase.include? q.downcase)
           if not @news.include?(a)
             @news.push(a)
           end
@@ -29,6 +29,24 @@ class HomeController < ApplicationController
         end
       end
      end
+
+     @fotos = Array.new
+     Foto.all.each do |a|
+        query.each do |q|
+        if (a.descripcion.downcase.include? q.downcase)
+          if not @fotos.include?(a)
+            @fotos.push(a)
+          end
+        end
+      end
+     end
+
+
+     if ((@fotos.count.eql? 0) or (@asociados.count.eql? 0) or (@news.count.eql? 0))
+      @noresultados = true
+    else
+      @noresultados = false
+    end
 
 
 
